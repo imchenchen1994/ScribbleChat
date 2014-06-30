6e5453d59d7ce72f5569ecf2bebc73414e419799
tze cheng <imchenchen1994@hotmail.com>
2014-06-30 11:50:49 +0800

    -a

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://<user>:<pass>@novus.modulusmongo.net:27017/gyveM6es' // esuqO3zo looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+}
---
+++ server.js
@@ -0,0 +1,103 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var server = require('http').Server(app);
+var configDB = require('./config/database.js');
+var io = require('socket.io')(server);
+
+// configuration ===============================================================
+mongoose.connect('mongodb://localhost/test'); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+app.configure(function() {
+
+	// set up our express application
+	app.use(express.logger('dev')); // log every request to the console
+	app.use(express.cookieParser()); // read cookies (needed for auth)
+	app.use(express.bodyParser()); // get information from html forms
+	app.set('view engine', 'ejs'); // set up ejs for templating
+  app.use(express.static(__dirname + '/app'));
+	// required for passport
+	app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+	app.use(flash()); // use connect-flash for flash messages stored in session
+});
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport,express,io); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+server.listen(port);
+console.log('The magic happens on port ' + port);
+
+var usernames = {};
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+})
